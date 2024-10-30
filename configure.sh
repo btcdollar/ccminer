ab79e0a82b9b11395dbc19ae670c9790c12cd3b6
Daniel <163345545+btcdollar@users.noreply.github.com>
2024-10-30 09:23:44 +0700

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -7,7 +7,7 @@ model_name=$(lscpu | awk -F ': +' '/Model name:/ { print $2 }')
 # Common compiler flags
 common_flags="-O3 -ffinite-loops -ffast-math -D_REENTRANT -finline-functions -falign-functions=16 -fomit-frame-pointer -fpic -pthread -flto -fuse-ld=lld -fno-stack-protector"
 
-cpu_flags="-march=armv8-a+crypto -mtune=cortex-X1 -mtune=cortex-a55 -mtune=cortex-a76"
+cpu_flags="-march=armv8-a+crypto -mtune=cortex-x1 -mtune=cortex-a55 -mtune=cortex-a76"
 # Set vectorization flags
 vectorization_flags="-Rpass-missed=loop-vectorize -Rpass-analysis=loop-vectorize -Wl"
 
